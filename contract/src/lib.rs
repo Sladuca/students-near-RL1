314bb5ca0fab08f795852359e17a19f18e17c7ec
Sebastien La Duca <sladuca777@gmail.com>
2020-05-09 17:10:02 -0400

    implement basic cache functionality

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -2,9 +2,7 @@ use near_sdk::{near_bindgen, env};
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use chrono::prelude::*;
-
-// -------- GEOCACHE -------- //
+use chrono::{Utc};
 
 #[derive(PartialEq, PartialOrd, Eq, Hash, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
 pub struct LogEntry {
@@ -12,33 +10,15 @@ pub struct LogEntry {
     date: String, // date signed
     message: String, // a friendly message :)
 }
-
-#[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(Default, Hash, BorshDeserialize, BorshSerialize)]
 pub struct Geocache {
     log: Vec<LogEntry>,
     owner: String,
-    account_id: String,
+    name: String,
 }
-
-#[near_bindgen]
-impl Geocache {
-    pub fn sign_in(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-        self.log.push(LogEntry { 
-            geocacher: account_id,
-            date: Utc::now().to_string(),
-            message: message,
-        });
-    }
-
-    // trade
-}
-
-// -------- GEOCOIN  -------- //
 
 #[derive(PartialEq, PartialOrd, Eq, Hash, Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
-pub struct GeoCoin {
+pub struct Geode {
     holder: String,
     bio: String,
     creator: String,
@@ -46,151 +26,192 @@ pub struct GeoCoin {
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct GeoCoinContract {
+pub struct GeodesContract {
     counter: u64,
-    coins: HashMap<u64, GeoCoin>,
-    coins_by_owner: HashMap<String, Vec<u64>>,
+    caches: HashMap<String, Geocache>,
+    geodes: HashMap<u64, Geode>,
+    geodes_by_owner: HashMap<String, Vec<u64>>,
     allowances: HashMap<String, Vec<u64>>,
+    
 }
 
 #[near_bindgen]
-impl GeoCoinContract {
+impl GeodesContract {
+
+    pub fn sign_cache(&mut self, cache_id: String, message: String) -> bool {
+        let logEntry = LogEntry {
+            geocacher: env::signer_account_id(),
+            date: Utc::now().to_string(),
+            message: message
+        };
+        match self.caches.get_mut(cache_id) {
+            Some(cache) => {
+                cache.log.push(logEntry);
+                true
+            }
+            None => false
+        }
+    }
+
+    pub fn add_geode_to_cache(&mut self, cache_id: String, geode_id: u64) -> bool {
+        // remove from owner's list
+        match self.geodes_by_owner.get(&env::signer_account_id()) {
+            Some(geodes) => match geodes.binary_search(geode_id) {
+                Ok(i) => geodes.remove(i),
+                Err(_) => return false,
+            }
+            None => return false
+        }
+        // do transfew
+        self.do_transfer(cache_id, env::signer_account_id(), geode_id)
+    }
+
+    pub fn trade_with_cache(&mut self, cache_id: String, give: u64, take: u64) -> bool {
+        // transfer give
+        match self.transfer(cache_id, give) {
+            true => {},
+            false => return false
+        };
+        // approve take
+        self.approve(env::signer_account_id(), take)
+    }
+
     pub fn mint_new(&mut self, bio: String) -> Option<u64> {
         if bio.chars().count() > 140 {
             return None
         }
         let id = self.counter;
-        self.coins.insert(id, GeoCoin {
+        self.geodes.insert(id, Geode {
             holder: env::signer_account_id(),
             bio: bio.clone(),
             creator: env::signer_account_id(),
         });
-        match self.coins_by_owner.get_mut(&env::signer_account_id()) {
-            Some(coins) => {
-                coins.push(id);
+        match self.geodes_by_owner.get_mut(&env::signer_account_id()) {
+            Some(geodes) => {
+                geodes.push(id);
             }
             None => {
-                let coins = vec![id];
-                self.coins_by_owner.insert(env::signer_account_id(), coins);
+                let geodes = vec![id];
+                self.geodes_by_owner.insert(env::signer_account_id(), geodes);
             }
         };
         self.counter += 1;
         Some(id)
-    }   
+    }
 
-    pub fn mint_copy(&mut self, coin_id: u64) -> Option<u64> {
-        let bio = match self.coins.get(&coin_id) {
-            Some(coin) => coin.bio.clone(),
+    pub fn mint_copy(&mut self, geode_id: u64) -> Option<u64> {
+        let bio = match self.geodes.get(&geode_id) {
+            Some(geode) => geode.bio.clone(),
             None => return None
         };
         let id = self.counter;
-        self.coins.insert(id, GeoCoin {
+        self.geodes.insert(id, Geode {
             holder: env::signer_account_id(),
             bio: bio,
             creator: env::signer_account_id(),
         });
-        match self.coins_by_owner.get_mut(&env::signer_account_id()) {
-            Some(coins) => {
-                coins.push(id);
+        match self.geodes_by_owner.get_mut(&env::signer_account_id()) {
+            Some(geodes) => {
+                geodes.push(id);
             }
             None => {
-                let coins = vec![id];
-                self.coins_by_owner.insert(env::signer_account_id(), coins);
+                let geodes = vec![id];
+                self.geodes_by_owner.insert(env::signer_account_id(), geodes);
             }
         };
         self.counter += 1;
         Some(id)
     }
     
-    pub fn approve(&mut self, spender: String, coin_id: u64) -> bool {
-        // make sure coin exists
-        match self.coins.get(&coin_id) {
+    pub fn approve(&mut self, spender: String, geode_id: u64) -> bool {
+        // make sure geode exists
+        match self.geodes.get(&geode_id) {
             Some(_) => {},
             None => return false
         };
         match self.allowances.get_mut(&spender) {
-            Some(coins) => coins.push(coin_id),
+            Some(geodes) => geodes.push(geode_id),
             None => {
-                let coins = vec![coin_id];
-                self.allowances.insert(spender, coins);
+                let geodes = vec![geode_id];
+                self.allowances.insert(spender, geodes);
             }
         };
         true
     }
 
-    fn do_transfer(&mut self, to: String, from: String, coin_id: u64) -> bool {
-        // update coin holder
-        let coin = match self.coins.get_mut(&coin_id) {
-            Some(coin) => {
-                coin.holder = to.clone();
-                coin
+    fn do_transfer(&mut self, to: String, from: String, geode_id: u64) -> bool {
+        // update geode holder
+        let geode = match self.geodes.get_mut(&geode_id) {
+            Some(geode) => {
+                geode.holder = to.clone();
+                geode
             },
             None => return false
         };
-        // remove from from's coins
-        match self.coins_by_owner.get_mut(&from) {
-            Some(coins) => {
-                let i = match coins.binary_search(&coin_id) {
+        // remove from from's geodes
+        match self.geodes_by_owner.get_mut(&from) {
+            Some(geodes) => {
+                let i = match geodes.binary_search(&geode_id) {
                     Ok(i) => i,
                     Err(_) => return false
                 };
-                coins.remove(i);
+                geodes.remove(i);
             }
             None => {
-                // undo changing of coin & return early
-                coin.holder = from;
+                // undo changing of geode & return early
+                geode.holder = from;
                 return false
             }
         };
-        // add to to's coins
-        match self.coins_by_owner.get_mut(&to) {
-            Some(coins) => {
-                match coins.binary_search(&coin_id) {
-                    Ok(_) => return true, // already holds the coin
-                    Err(_) => {} // doesn't hold the coin yet
+        // add to to's geodes
+        match self.geodes_by_owner.get_mut(&to) {
+            Some(geodes) => {
+                match geodes.binary_search(&geode_id) {
+                    Ok(_) => return true, // already holds the geode
+                    Err(_) => {} // doesn't hold the geode yet
                 };
-                coins.push(coin_id);
+                geodes.push(geode_id);
             },
             None => {
-                let coins = vec![coin_id];
-                self.coins_by_owner.insert(to, coins);
+                let geodes = vec![geode_id];
+                self.geodes_by_owner.insert(to, geodes);
             }
         };
         return true
     }
 
-    pub fn transfer_from(&mut self, to: String, from: String, coin_id: u64) -> bool {
+    pub fn transfer_from(&mut self, to: String, from: String, geode_id: u64) -> bool {
         // check to make sure allowance exists
         match self.allowances.get_mut(&env::signer_account_id()) {
-            Some(coins) => match coins.binary_search(&coin_id) {
+            Some(geodes) => match geodes.binary_search(&geode_id) {
                 Ok(i) => {
-                    coins.remove(i);
+                    geodes.remove(i);
                 }
                 Err(_) => return false
             },
             None => return false
         };
-        let res = self.do_transfer(to, from, coin_id);
+        let res = self.do_transfer(to, from, geode_id);
         if !res {
             // panic if we can't repair the state
-            let coins = self.allowances.get_mut(&env::signer_account_id()).unwrap();
-            coins.push(coin_id);
+            let geodes = self.allowances.get_mut(&env::signer_account_id()).unwrap();
+            geodes.push(geode_id);
             return false
         }
         true
     }
 
-    pub fn transfer(&mut self, to: String, coin_id: u64) -> Option<u64> {
-        // make sure coin exists and is owned by signer_account_id
-        match self.coins.get_mut(&coin_id) {
-            Some(coin) => match coin.holder == env::signer_account_id() {
+    pub fn transfer(&mut self, to: String, geode_id: u64) -> Option<u64> {
+        // make sure geode exists and is owned by signer_account_id
+        match self.geodes.get_mut(&geode_id) {
+            Some(geode) => match geode.holder == env::signer_account_id() {
                 true => {},
                 false => return None,
             },
             None => return None,
         };
-        match self.do_transfer(to, env::signer_account_id(), coin_id) {
-            true => Some(coin_id),
+        match self.do_transfer(to, env::signer_account_id(), geode_id) {
+            true => Some(geode_id),
             false => None
         }
     }
