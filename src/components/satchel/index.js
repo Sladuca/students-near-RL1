927fdbc616280edb31764b5acf63dfdd05fc35d7
Sebastien La Duca <sladuca777@gmail.com>
2020-05-12 12:48:33 -0400

    add mintNew (doesnt work yet)

--- src/components/satchel/index.js
+++ src/components/satchel/index.js
@@ -5,9 +5,16 @@ class Satchel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      login: false,
       satchel: [],
+      bioInput: ''
     }
+  }
+
+  updateText(e) {
+    this.setState({
+      ...this.state,
+      bioInput: e.target.value
+    });
   }
 
   async getGeodes() {
@@ -21,11 +28,37 @@ class Satchel extends React.Component {
     this.setState({
       ...this.state,
       satchel: geodes
-    })
+    });
+  }
+
+  async mintNew() {
+    const bio = this.state.bioInput;
+    if (bio.length > 280) {
+      console.log('must be <= 280 characters!');
+      return;
+    }
+    const geode_id = await this.props.contract.mint_new({ bio });
+    if (!geode_id) {
+      console.log('mint failed!')
+      return;
+    }
+    const geode = await this.props.contract.get_geode({ geode_id });
+    if (!geode) {
+      console.log('geode doesnt exist???')
+      return;
+    }
+    this.setState({
+      ...this.state,
+      satchel: [...this.state.satchel, geode]
+    });
   }
 
   render() {
-    return <Presenter satchel={this.state.satchel} getGeodes={async (e) => this.getGeodes()}/>
+    return <Presenter satchel={this.state.satchel} getGeodes={async (e) => this.getGeodes()} mintNew={async (e) => {
+      if (e.key === 'Enter') {
+        await this.mintNew()
+      }
+    }} updateText={(e) => this.updateText(e)}/>
   }
 }
 
--- src/components/satchel/presenter.js
+++ src/components/satchel/presenter.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default ({ satchel, getGeodes }) => {
+export default ({ satchel, getGeodes, updateText, mintNew }) => {
   return (
     <>
+      <input onChange={updateText} onKeyUp={mintNew} placeholder="new geode bio here!"></input>
       <label htmlFor="getGeodes">get geodes!</label>
       <button id="getGeodes" onMouseUp={getGeodes}></button>
       <ol>
