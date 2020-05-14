8268aad9807d4b46beaade4bae914d630b318d4e
Lexi McQueen <alexis.mcqueen@otterbein.edu>
2020-05-13 21:58:33 -0400

    Fixes to getCache and Geocache constructor.

--- src/components/geocache/index.js
+++ src/components/geocache/index.js
@@ -7,7 +7,7 @@ class Geocache extends React.Component {
     this.state = {
       login: false,
       cacheSearch: '',
-      cache: cache,
+      cache: {},
     }
   }
 
@@ -21,8 +21,8 @@ class Geocache extends React.Component {
   async getCache() {
     //get geocache data: log, owner, name
     const cache_id = await this.props.contract.get_cache({cache_id: id_from_text });
-    e
-    this.state.cache.log = {};
+  
+    this.state.cache.log = '';
     this.state.cache.owner = '';
     this.state.cache.name = '';
     console.log(this.cache_id.log); // not sure about this line. want to get the log from the cache
